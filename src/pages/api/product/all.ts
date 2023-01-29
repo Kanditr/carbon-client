79861369d946e5399bb391261fae22553a9f41f4
Kanditr <gun.rodwong@gmail.com>
2023-01-29 16:55:51 +0700

    TE-4 Implement apikey to server

--- src/pages/api/product/all.ts
+++ src/pages/api/product/all.ts
@@ -9,7 +9,8 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       // Get data from api
       try {
         const response = await axios.get(
-          `${process.env.API_BASE_URL}/v1/product/all`
+          `${process.env.API_BASE_URL}/v1/product/all`,
+          { headers: { apikey: `${process.env.API_KEY}` } }
         );
         res.status(200).json(response.data);
       } catch (error: any) {
